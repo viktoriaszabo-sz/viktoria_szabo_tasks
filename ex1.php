c6da40767b0cd938623c9bdaa3b62e2bb9bc310f
viktoriaszabo-sz <szaboviktoria.sz03@gmail.com>
2023-02-06 00:01:31 +0200

    Create ex1.php

---
+++ ex1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first php page</title>
+</head>
+<body>
+    <h1>My simple php script</h1>
+
+    <?php
+
+    echo "Hello World";
+    echo "this is new line";
+    echo "this is how to escape spec char 'something'";
+    echo "this is how to escape spec chair \"to close\"";
+
+    ?>
+
+</body>
+</html>
+
