07ce5e6cea6a78fc02fc52bf4dfc59f388077906
viktoriaszabo-sz <szaboviktoria.sz03@gmail.com>
2023-02-06 16:08:17 +0200

    assignment 1 done

--- ex1.php
+++ ex1.php
@@ -19,7 +19,51 @@
     ?>
 
     <h1>3.3 Write the PHP code in to display the current date </h1>
-    <?php echo date("d.m.y")?>
+    <?php echo date("d.M.Y")?>
+
+    <h1>3.4 $title = "PHP is interesting”</h1>
+    <?php
+    $title = "PHP is interesting";
+    echo "<h1>" . $title . "</h1>"
+    ?>
+
+    <h1>3.5 Use HTML table into echo and include 3 columns S.n., Name and grade</h1>
+
+    <?php 
+    
+    $g1 = 5;
+    $g2 = 4;
+    $g3 = 5;
+
+    echo "
+    <table>
+
+    <tr>
+        <th> S.n. </th>
+        <th> Name </th>
+        <th> Grade </th>
+    </tr>
+    <td> 1 </td> 
+    <td> Pekka </td> 
+    <td> $g1 </td> 
+    </tr>
+    <td> 2 </td> 
+    <td> Johanna </td> 
+    <td> $g2 </td> 
+    </tr>
+    <td> 3 </td>
+    <td> John </td>  
+    <td> $g3 </td> 3
+    </tr>
+
+    </table>
+    "
+    ?> 
+
+    <h1>Take a screenshot that verifies, you have set up the development environment and include it as an image in ex1.php. 
+</h1>
+
+    <img src = "devs.png" alt ="image">
 
 </body>
 </html>
